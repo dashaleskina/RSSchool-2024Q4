e76546d3b8e3b59793530d31c95d657b1779e957
Daria Leskina <dleskinaleskina@yandex.ru>
2024-11-30 14:51:41 +0300

    feat: add function of switching categories

--- christmas-shop/scripts/gifts.js
+++ christmas-shop/scripts/gifts.js
@@ -1,17 +1,51 @@
 import { createCard } from "./utils.js";
 
-
-const giftsCardsList = document.querySelector(".gifts__cards-list");
+//находим необходимые нам элементы
+const giftsCardsList = document.querySelector(".gifts__cards-list"); // список для карточек
+const categoryTabs = document.querySelectorAll(".gifts__tabs-item");
 
 //отрисовываем карточки на странице gifts
 fetch("../scripts/gifts.json")
   .then((response) => response.json())
   .then((data) => {
-    data.forEach((cardData) => {
-      const card = createCard(cardData);
-      giftsCardsList.appendChild(card);
-    });
+      displayCards(data);
+      handleCategoryTabs(data);
   })
   .catch((error) => {
     console.error("Ошбика при загрузке данных", error);
   });
+
+// функция для отрисовки карточек на странице - очищаем список  и далее вставляем карточки из массива
+function displayCards(cards) {
+  giftsCardsList.innerHTML = "";
+  cards.forEach((cardData) => {
+    const card = createCard(cardData);
+    giftsCardsList.appendChild(card);
+  });
+}
+
+//обработаем клики на табы
+function handleCategoryTabs(data) {
+  categoryTabs.forEach((item) => {
+    item.addEventListener("click", () => {
+      let sortedCards;
+      const categoryText = item.textContent.toLowerCase();
+
+      if (categoryText === "all") {
+        sortedCards = data;
+      } else {
+        sortedCards = data.filter((card) =>
+          card.category.toLowerCase().includes(categoryText)
+        );
+      }
+
+      displayCards(sortedCards);
+
+      //удаляем активный класс у всех кнопки и переключаем на новую
+      categoryTabs.forEach((item) =>
+        item.classList.remove("gifts__tabs-item--active")
+      );
+      item.classList.add("gifts__tabs-item--active");
+    });
+  });
+}
