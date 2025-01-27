7abb03cad3eb728f700262ef65dffc144685dfe3
Daria Leskina <dleskinaleskina@yandex.ru>
2025-01-27 11:49:25 +0300

    feat: add functions of rendering gamepad field

--- nonograms/script/script.js
+++ nonograms/script/script.js
@@ -1,8 +1,32 @@
 //imports
 import { schemes } from "./schemes.js";
+import { setSizeOfField, setSizeOfCell } from "./options.js";
+
+//variables
+const nonogramField = document.createElement("div");
+nonogramField.className = "nonogramField";
 
 function createStartScreen(container, difficulty) {
-  //тут будет логика вставки элементов в контейнер геймпада
+  container.appendChild(createFieldSize(difficulty, nonogramField));
+}
+
+function createFieldSize(difficulty, field) {
+  field.innerHTML = "";
+
+  let sizeOfField = setSizeOfField(difficulty);
+  let sizeOfCell = setSizeOfCell(difficulty);
+  field.style.width = `${sizeOfField * sizeOfCell}px`;
+
+  for (let i = 0; i < sizeOfField * sizeOfField; i++) {
+    const cell = document.createElement("div");
+    cell.className = "cell";
+
+    cell.style.width = `${sizeOfCell}px`;
+    cell.style.height = `${sizeOfCell}px`;
+    field.appendChild(cell);
+  }
+
+  return field;
 }
 
 function initGame() {
@@ -12,7 +36,7 @@ function initGame() {
     gamepad.className = "gamepad";
     document.body.appendChild(gamepad);
   }
-  createStartScreen(gamepad, "easy");
+  createStartScreen(gamepad, "hard");
 }
 
 initGame();
