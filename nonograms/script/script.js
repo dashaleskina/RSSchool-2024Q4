e8f213ec7eab1ead0efe7a63a763de30f8520aee
Daria Leskina <dleskinaleskina@yandex.ru>
2025-01-30 15:06:46 +0300

    feat: add info block that informs about the victory

--- nonograms/script/script.js
+++ nonograms/script/script.js
@@ -15,9 +15,9 @@ let lengthOfFirstLine;
 let indexOfSchema = 0;
 let gameStarted = false;
 let flatArray = schemes[0].flat();
-let flatArrayForCheck = flatArray.map(item => {
-    return item === 1 ? 0 : item;
-})
+let flatArrayForCheck = flatArray.map((item) => {
+  return item === 1 ? 0 : item;
+});
 
 const nonogramField = document.createElement("div");
 nonogramField.className = "nonogramField";
@@ -53,6 +53,14 @@ resetButton.classList = "resetButton";
 resetButton.textContent = "Reset";
 menu.appendChild(resetButton);
 
+const footerBlock = document.createElement("div");
+footerBlock.classList = "footerBlock"
+
+const infoMessage = document.createElement("div");
+infoMessage.classList = "infoMessage";
+infoMessage.textContent = "Great! You have solved the nonogram!";
+footerBlock.appendChild(infoMessage);
+
 function createStartScreen(container, difficulty, number) {
   topLevelOfField.appendChild(createTopHintsField(difficulty, number));
 
@@ -60,7 +68,9 @@ function createStartScreen(container, difficulty, number) {
 
   bottomLevelOfField.appendChild(createLeftHintsField(difficulty, number));
   bottomLevelOfField.appendChild(createFieldSize(difficulty, nonogramField)); //само поле
+
   container.appendChild(bottomLevelOfField);
+  container.appendChild(footerBlock);
 }
 
 function createLeftHintsField(difficulty, number) {
@@ -173,84 +183,84 @@ function createFieldSize(difficulty, field) {
 // }
 
 function checkSchemaAnswer(index) {
-    flatArrayForCheck[index] = 1;
-    if (JSON.stringify(flatArrayForCheck) === JSON.stringify(flatArray)) {
-        console.log('correct');
-    } else {
-        console.log('continue', flatArray, flatArrayForCheck)
-
-    };
+  flatArrayForCheck[index] = 1;
+  if (JSON.stringify(flatArrayForCheck) === JSON.stringify(flatArray)) {
+    infoMessage.style.display = "flex";
+    resetButton.disabled = true;
+  } else {
+    console.log("continue");
+  }
 }
 
 function initGame(difficulty, number) {
-    let gamepad = document.querySelector(".gamepad");
-    if (!gamepad) {
-      gamepad = document.createElement("div");
-      gamepad.className = "gamepad";
-      document.body.appendChild(gamepad);
-    }
-    createStartScreen(gamepad, difficulty, number);
-    
-    // После создания нового поля, добавляем обработчики событий
-    addCellEventListeners();
+  let gamepad = document.querySelector(".gamepad");
+  if (!gamepad) {
+    gamepad = document.createElement("div");
+    gamepad.className = "gamepad";
+    document.body.appendChild(gamepad);
   }
-  
-  // Функция для добавления обработчиков событий к ячейкам
-  function addCellEventListeners() {
-    const cellsArray = document.querySelectorAll(".nonogramField .cell");
-    
-    // Меняем цвета
-    cellsArray.forEach((cell, index) => {
-      cell.addEventListener("click", () => {
-        if (cell.classList.contains("shadedCell")) {
-          cell.classList.remove("shadedCell");
-        } else {
-          cell.classList.remove("crossCell");
-          cell.classList.add("shadedCell");
-          checkSchemaAnswer(index);
-        }
-      });
-  
-      cell.addEventListener("contextmenu", (e) => {
-        e.preventDefault();
-        if (cell.classList.contains("crossCell")) {
-          cell.classList.remove("crossCell");
-        } else {
-          cell.classList.remove("shadedCell");
-          cell.classList.add("crossCell");
-        }
-      });
-    });
-  }
-  
-  // Слушатель для выбора схемы
-  select.addEventListener("change", () => {
-    selectedText = select.options[select.selectedIndex].text;
-    lengthOfFirstLine = schemes[select.selectedIndex][0].length;
-    chosenDifficulty =
-      lengthOfFirstLine === 5
-        ? "easy"
-        : lengthOfFirstLine === 10
-        ? "medium"
-        : lengthOfFirstLine === 15
-        ? "hard"
-        : chosenDifficulty;
-    console.log(lengthOfFirstLine, chosenDifficulty);
-    flatArray = schemes[select.selectedIndex].flat();
-    flatArrayForCheck = flatArray.map(item => {
-        return item === 1 ? 0 : item;
-    }) // Убедитесь, что вы получаете правильный массив
-    initGame(chosenDifficulty, select.selectedIndex);
-  });
-  
-  // Сброс игры
-  resetButton.addEventListener("click", () => {
-    const cellsArray = document.querySelectorAll(".nonogramField .cell"); // Обновляем массив ячеек
-    cellsArray.forEach((cell) => {
-      cell.classList.remove("shadedCell", "crossCell");
-    });
-  });
-  
+  createStartScreen(gamepad, difficulty, number);
 
+  addCellEventListeners();
+}
+
+// клик по клетке
+function addCellEventListeners() {
+  const cellsArray = document.querySelectorAll(".nonogramField .cell");
+
+  cellsArray.forEach((cell, index) => {
+    cell.addEventListener("click", () => {
+      if (cell.classList.contains("shadedCell")) {
+        cell.classList.remove("shadedCell");
+      } else {
+        cell.classList.remove("crossCell");
+        cell.classList.add("shadedCell");
+        checkSchemaAnswer(index);
+      }
+    });
+
+    cell.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      if (cell.classList.contains("crossCell")) {
+        cell.classList.remove("crossCell");
+      } else {
+        cell.classList.remove("shadedCell");
+        cell.classList.add("crossCell");
+      }
+    });
+  });
+}
+
+// выбор схемы
+select.addEventListener("change", () => {
+  selectedText = select.options[select.selectedIndex].text;
+  lengthOfFirstLine = schemes[select.selectedIndex][0].length;
+  chosenDifficulty =
+    lengthOfFirstLine === 5
+      ? "easy"
+      : lengthOfFirstLine === 10
+      ? "medium"
+      : lengthOfFirstLine === 15
+      ? "hard"
+      : chosenDifficulty;
+  console.log(lengthOfFirstLine, chosenDifficulty);
+  flatArray = schemes[select.selectedIndex].flat();
+  flatArrayForCheck = flatArray.map((item) => {
+    return item === 1 ? 0 : item;
+  });
+  infoMessage.style.display = "none"
+  initGame(chosenDifficulty, select.selectedIndex);
+});
+
+// сброс игры
+resetButton.addEventListener("click", () => {
+  const cellsArray = document.querySelectorAll(".nonogramField .cell"); // Обновляем массив ячеек
+  cellsArray.forEach((cell) => {
+    cell.classList.remove("shadedCell", "crossCell");
+  });
+  flatArrayForCheck = flatArray.map((item) => {
+    return item === 1 ? 0 : item;
+  });
+});
 
 initGame(chosenDifficulty, indexOfSchema);
